dbef47fb8b4ab9b7d681d2dd5438c6ca30d1381b
shrey217 <63427797+shrey217@users.noreply.github.com>
2025-08-19 13:21:57 +0530

    Update app.py

--- projects/Poker Auction/app.py
+++ projects/Poker Auction/app.py
@@ -10,5 +10,28 @@ def home():
 def dashboard():
     return render_template('dashboard.html')
 
+@app.route('/auction')
+def auction():
+    return render_template('auction_table.html')
+
+@app.route('/auction_table')
+def auction_table():
+    return render_template('auction_table.html')
+
+@app.route('/pref')
+def pref():
+    return render_template('pref.html')
+
+@app.route('/preferences')
+def preferences():
+    return render_template('pref.html')
+
+@app.route('/teams')
+def teams():
+    return render_template('teams_view.html')
+
+@app.route('/teams_view')
+def teams_view():
+    return render_template('teams_view.html')
 if __name__ == '__main__':
     app.run(debug=True)
